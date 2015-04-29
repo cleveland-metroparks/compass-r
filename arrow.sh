39e7bc96c908d248c2e56a223c338d868acebd3f
Stephen Vincent Mather <svm@clevelandmetroparks.com>
2015-04-29 12:23:21 -0400

    create

---
+++ arrow.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+for image in `ls *.JPG`
+do
+	# trim off leading and trailing spaces in image variable
+	image=`echo $image | xargs`
+	# get non-extension portion of name
+	name=`echo $image | awk -F'.' '{print $1}'` 	
+	# extract angle from EXIF, and turn into variable
+	angle=`exif $image | grep 'Image Direction' | awk 'BEGIN { FS = "|" } ; { print $2 }' | sed 's/M//g' | xargs`
+	# calculate angle in cartesian coordinates
+	angle=`echo 90 - $angle | bc`
+	
+	# create rotated and scaled arrow for image
+	convert -rotate $angle -transparent white -resize 300x300 arrow-hi.png arrow-hi_$angle.png
+	# overlay that arrow on original image
+	composite arrow-hi_$angle.png $image $name.png
+	# perform a wee bit of cleanup
+	rm arrow-hi_$angle.png
+done
